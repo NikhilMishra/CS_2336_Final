097d66bf7a9d397ec35313ab7fc8eda0edeb9262
NikhilMishra <mishranikh@gmail.com>
2019-05-05 16:13:48 -0700

    shortest path and mst

--- mainDriver.java
+++ mainDriver.java
@@ -25,90 +25,9 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.layout.Pane;
 import javafx.geometry.Point2D;
-import javafx.scene.layout.FlowPane;
+
 
 public class mainDriver extends Application {
-	@Override
-	public void start(Stage stage) throws Exception {
-
-
-		// the pane for the graph
-		Pane circlePane = new CirclePane();
-		circlePane.setStyle(" -fx-border-width: 5");
-		circlePane.setPrefHeight(350);
-		circlePane.setPrefWidth(805);
-		
-		// pane for shortest path button
-		FlowPane spPane = new FlowPane();
-		Label startVertex = new Label("Starting vertex:");
-		TextField startVertexField = new TextField();
-		startVertexField.setPrefWidth(40);
-		Label endVertex = new Label("Ending vertex:");
-		TextField endVertexField = new TextField();
-		endVertexField.setPrefWidth(40);
-		Button shortestPathButton = new Button("Show Shortest Path");
-		spPane.setHgap(5);
-		spPane.getChildren().addAll(startVertex,
-				startVertexField, endVertex, endVertexField,
-				shortestPathButton);
-		Label shortestPathLabel = new Label("Find a shortest path");
-		BorderPane shortestPathPane = new BorderPane();
-		shortestPathPane.setTop(shortestPathLabel);
-		shortestPathPane.setBottom(spPane);
-		shortestPathPane.setStyle(" -fx-border-width: 5");
-		shortestPathPane.setPadding(new Insets(5, 5, 5, 5));
-		
-		// pane for other buttons
-		FlowPane buttonsPane = new FlowPane();
-		buttonsPane.setAlignment(Pos.BOTTOM_CENTER);
-		buttonsPane.setPadding(new Insets(10, 10, 10, 10));
-		buttonsPane.setHgap(10);
-		buttonsPane.setVgap(10);
-		Button showMSTButton = new Button("Show MST");
-		Label sourceVertexLabel = new Label("Source vertex: ");
-		TextField sourceVertexField = new TextField();
-		sourceVertexField.setPrefWidth(40);
-		Button showAllSPButton = new Button("Show All SP From the Source");
-		buttonsPane.getChildren().addAll(showMSTButton, sourceVertexLabel,
-				sourceVertexField, showAllSPButton, shortestPathPane);
-		
-		// pane for instructions
-		GridPane instructionPane = new GridPane();
-		instructionPane.setPadding(new Insets(10, 10, 10, 10));
-		Label instructTopLabel = new Label("INSTRUCTIONS");
-		Label instructLeftLabel1 = new Label("Add:");
-		Label instructLeftLabel2 = new Label("Move:");
-		Label instructLeftLabel3 = new Label("Connect:");
-		Label instructLeftLabel4 = new Label("Remove:");
-		Label instructRightLabel1 = new Label("Left Click");
-		Label instructRightLabel2 = new Label("Ctrl Drag");
-		Label instructRightLabel3 = new Label("Drag");
-		Label instructRightLabel4 = new Label("Right Click");
-		instructionPane.add(instructTopLabel, 0, 0);
-		instructionPane.add(instructLeftLabel1, 0, 1);
-		instructionPane.add(instructLeftLabel2, 0, 2);
-		instructionPane.add(instructLeftLabel3, 0, 3);
-		instructionPane.add(instructLeftLabel4, 0, 4);
-		instructionPane.add(instructRightLabel1, 1, 1);
-		instructionPane.add(instructRightLabel2, 1, 2);
-		instructionPane.add(instructRightLabel3, 1, 3);
-		instructionPane.add(instructRightLabel4, 1, 4);
-		
-		// Align panes
-		BorderPane rootPane = new BorderPane();
-		rootPane.setBottom(buttonsPane);
-		rootPane.setRight(circlePane);
-		rootPane.setLeft(instructionPane);
-
-		Scene scene = new Scene(rootPane, 1200, 400);
-		stage.setTitle("Weighted Graph");
-		stage.setMinHeight(500);
-		stage.setMaxHeight(500);
-		stage.setMinWidth(1000);
-		stage.setMaxWidth(1000);
-		stage.setScene(scene);
-		stage.show();
-	}
 	
 	//User-clicked vertices on graph
 	private ArrayList<Point2D> points = new ArrayList<>();
@@ -118,6 +37,31 @@ public class mainDriver extends Application {
 	
 	//User-created tree
 	private AbstractGraph<Integer>.Tree tree = null;
+	
+	//Creating buttons objects for Min. Spanning tree, Shortest Path from source,
+	//and Shortest Path from specified points.
+	private Button mstButton = new Button("Show MST");
+	private Button spSrcButton = new Button("Show All SP From the Source");
+	private Button allSPButton = new Button("Show Shortest Path");
+	
+	//Textfields for Source Vertex, Starting vertex, and Ending vertex
+	private Label srcV = new Label("Source Vertex");
+	private TextField srcVertexTxt = new TextField();
+	
+	private Label startV = new Label("Starting Vertex");
+	private TextField startVertexTxt = new TextField();
+	
+	private Label endV = new Label("Ending Vertex");
+	private TextField endVertexTxt = new TextField();
+	
+	private Label spLab = new Label("Find Shortest Path");
+	
+	//Instruction Labels
+	private Label instructTitle = new Label("INSTRUCTIONS:");
+	private Label instructAdd = new Label("Add:			Left Click");
+	private Label instructMove = new Label("Move:		Ctrl Drag");
+	private Label instructConnect = new Label("Connect:		Drag");
+	private Label instructRemove = new Label("Remove:		Right Click");
 	
 	public class Line {
 		
@@ -140,7 +84,18 @@ public class mainDriver extends Application {
 	}
 	
 	private WeightedGraph<Integer> getGraph() {
-		
+		/*Integer[] vertice = {0, 1, 2, 3, 4};
+
+		int[][] edgess =     {{0, 1, 2}, {0, 3, 8}, 
+			      {1, 0, 2}, {1, 2, 7}, {1, 3, 3},
+			      {2, 1, 7}, {2, 3, 4}, {2, 4, 5},
+			      {3, 0, 8}, {3, 1, 3}, {3, 2, 4}, {3, 4, 6},
+			      {4, 2, 5}, {4, 3, 6}};
+		WeightedGraph<Integer> graph1 = new WeightedGraph<>(vertice, edgess);
+		for (int i = 0; i < points.size(); i++) {
+			return graph1;*/
+
+
 		  List<Integer> vertices = new ArrayList<>();
 		  
 		  for (int i = 0; i < points.size(); i++) {
@@ -178,6 +133,164 @@ public class mainDriver extends Application {
 	BorderPane pane = new BorderPane();
 
 	@SuppressWarnings({ "static-access", "rawtypes", "unchecked" })
+	@Override
+	public void start(Stage stage) throws Exception {
+		//if shortest path button is pressed
+		allSPButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				//get user values
+				Integer value1 = Integer.valueOf(startVertexTxt.getText());
+				Integer value2 = Integer.valueOf(endVertexTxt.getText());
+				//initialize the graph
+				WeightedGraph thisGraph = getGraph();
+				WeightedGraph<String>.ShortestPathTree tree1 = thisGraph.getShortestPath(getGraph().getIndex(value2));
+				java.util.List<String> path = tree1.getPath(getGraph().getIndex(value1));
+				//print the path
+				for (String s : path) {
+					System.out.print(s + " ");
+				}
+				System.out.println();
+			}
+		});
+		//if mst button is pressed
+		mstButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				//initialize weighted graph
+				WeightedGraph thisGraph = getGraph();
+				//if there are at least 2 vertices
+				if ((thisGraph.getVertices()).size() >= 2) {
+					//print tree
+					WeightedGraph<String>.MST tree1 = thisGraph.getMinimumSpanningTree();
+					tree1.printTree();}
+			}
+		});
+		//Set text field space
+		srcVertexTxt.setPrefColumnCount(2);
+		startVertexTxt.setPrefColumnCount(2);
+		endVertexTxt.setPrefColumnCount(2);  
+		
+		//Instructions
+		VBox instruct = new VBox();
+				
+		instruct.getChildren().addAll(instructTitle, instructAdd, instructMove, instructConnect, instructRemove);
+		
+		instruct.setMargin(instructTitle, new Insets(10, 0, 5, 20));
+		instruct.setMargin(instructAdd, new Insets(10, 0, 5, 20));
+		instruct.setMargin(instructMove, new Insets(10, 0, 5, 20));
+		instruct.setMargin(instructConnect, new Insets(10, 0, 5, 20));
+		instruct.setMargin(instructRemove, new Insets(10, 0, 5, 20));
+		
+		instruct.setStyle("-fx-padding: 10;" + 
+                "-fx-border-style: solid inside;" + 
+                "-fx-border-width: 2;" +
+                "-fx-border-height: 5;" +
+                "-fx-border-insets: 5;" + 
+                "-fx-border-radius: 5;" + 
+                "-fx-border-color: black;");
+		
+		pane.setLeft(instruct);
+		BorderPane.setAlignment(instruct, Pos.TOP_LEFT);
+				
+	
+
+		//Starting vert/Ending Vert
+		HBox boxRight = new HBox(5);
+		boxRight.setStyle("-border-color: brown");
+		
+		boxRight.getChildren().addAll(startV, startVertexTxt, endV , endVertexTxt, allSPButton);
+		
+		boxRight.setAlignment(Pos.CENTER);
+		 
+		
+		//HBox for left side options
+		HBox boxLeft = new HBox(5);
+				
+	    boxLeft.getChildren().addAll( mstButton, srcV, srcVertexTxt, spSrcButton);  
+	    
+	    boxLeft.setAlignment(Pos.CENTER);
+	    
+	    
+	    //Bottom box
+	    HBox bottomBox = new HBox();
+	    
+	    bottomBox.getChildren().addAll(boxLeft, boxRight);
+	    
+	    bottomBox.setMargin(boxLeft, new Insets(20, 20, 20, 20));
+	    
+	    bottomBox.setMargin(boxRight, new Insets(20, 20, 20, 20));
+	    
+	    pane.setBottom(bottomBox);
+	    
+
+	    
+	    pane.setOnMouseClicked(e-> {
+	    	
+            double x = e.getX();
+            double y = e.getY();
+
+            if (e.getButton() == MouseButton.PRIMARY) {
+            	
+                Circle c = drawPoint(x,y);
+                
+                pane.getChildren().add(c);
+                
+            } else if ( e.getButton() == MouseButton.SECONDARY ) {
+
+                removePoint(x, y);
+                
+            }
+
+
+        });
+	    
+	    //Creating a scene object 
+	    Scene scene = new Scene(pane, 980, 350); 
+	       
+	    //Setting title to the Stage 
+	    stage.setTitle("Weighted Graph"); 
+	         
+	    //Adding scene to the stage 
+	    stage.setScene(scene);
+	      
+	    //Displaying the contents of the stage 
+	    stage.show(); 
+	    
+	    
+	    
+		
+	}
+	
+	private Circle drawPoint(double x, double y) {
+		
+        Circle c = new Circle(x, y, 20, Color.TRANSPARENT);
+        
+        c.setStroke(Color.BLACK);
+        
+        return c;
+        
+    }
+	
+	private void removePoint(double x, double y) {
+
+        ObservableList<Node> list = pane.getChildren();
+        
+        for (int i = list.size() - 1; i >= 0; i--) {
+        	
+            Node c = list.get(i);
+
+            if (c instanceof Circle && c.contains(x, y)) {
+            	
+                pane.getChildren().remove(c);
+                
+                break;
+                
+            }
+            
+        }
+        
+    }
 	
 
 	public static void main(String[] args) {
